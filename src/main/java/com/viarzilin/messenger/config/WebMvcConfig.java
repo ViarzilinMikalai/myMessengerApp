6e46bc38885e00b25456f7dab6176665dfecdf0f
mikalai.viarzilin <vernik3@tut..by>
2020-11-10 15:56:06 +0300

    Add Vue-Router

---
+++ src/main/java/com/viarzilin/messenger/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.viarzilin.messenger.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return container ->{
+          container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+        };
+    }
+}
